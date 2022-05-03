2538212aee1c64f1765a611316a8af1e7f2206f6
Jose Ibañez <zegnhabi@gmail.com>
2022-05-03 04:16:07 -0500

    se agregan repetidos y valida totales.

--- screens/Sales/Sales.js
+++ screens/Sales/Sales.js
@@ -15,17 +15,86 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import { openDatabase } from "../../provider/db/sqlite";
 
 const db = openDatabase();
+const Button = ({ title, onPress, iconName }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    style={{
+      backgroundColor: Colors.yes,
+      padding: 5,
+      margin: 5,
+      borderRadius: 5,
+    }}
+  >
+    <View
+      style={{
+        flexDirection: "row",
+        height: 40,
+      }}
+    >
+      <Ionicons
+        name={iconName}
+        style={{
+          margin: 5,
+          padding: 5,
+          fontSize: 20,
+          color: Colors.white,
+        }}
+      />
+      <Text
+        style={{
+          margin: 5,
+          padding: 5,
+          fontSize: 15,
+          color: Colors.white,
+          fontWeight: "bold",
+        }}
+      >
+        {title}
+      </Text>
+    </View>
+  </TouchableOpacity>
+);
 
 export default function Sales() {
   const [productsForSales, setProductsForSales] = useState([]);
+  const onAddProduct = (product) => {
+    const existentProduct = productsForSales.find((p) => p.id === product.id);
+    if (existentProduct) {
+      console.log("Product already added");
+      onAddSaleProduct(product.id);
+    } else {
+      console.log("Product added");
+      setProductsForSales([...productsForSales, product]);
+    }
+  };
+
+  const onAddSaleProduct = (id) => {
+    const newProductsForSales = productsForSales.map((product) => {
+      if (product.id === id) {
+        product.quantity += 1;
+      }
+      return product;
+    });
+    setProductsForSales(newProductsForSales);
+  };
+
+  const onRemoveSaleProduct = (id) => {
+    const newProductsForSales = productsForSales
+      .map((product) => {
+        if (product.id === id) {
+          if (product.quantity > 0) {
+            product.quantity -= 1;
+          }
+        }
+        return product;
+      })
+      .filter((product) => product.quantity > 0);
+    setProductsForSales(newProductsForSales);
+  };
 
   function SearchProducts() {
     const [products, setProducts] = useState([]);
     const [filteredProducts, setFilteredProducts] = useState([]);
-
-    const onAddProduct = (product) => {
-      setProductsForSales([...productsForSales, product]);
-    };
 
     const fetchData = () => {
       console.log("Fetching data...", new Date());
@@ -114,7 +183,7 @@ export default function Sales() {
           paddingTop: 0,
           paddingHorizontal: 10,
           width: "100%",
-          maxHeight: 250,
+          maxHeight: 350,
         }}
       >
         <TextInput
@@ -181,7 +250,7 @@ export default function Sales() {
                   fontWeight: "bold",
                 }}
               >
-                {item.price} c/u
+                {item.price.toFixed(2)} c/u
               </Text>
             </View>
             <View
@@ -204,7 +273,7 @@ export default function Sales() {
                   fontWeight: "bold",
                 }}
               >
-                {item.price * item.quantity}
+                {(item.price * item.quantity).toFixed(2)}
               </Text>
             </View>
           </View>
@@ -213,15 +282,7 @@ export default function Sales() {
           style={{
             margin: 5,
           }}
-          onPress={() =>
-            onAddProduct({
-              id: item.id,
-              name: item.name,
-              price: item.price,
-              barcode: item.barcode,
-              quantity: 1,
-            })
-          }
+          onPress={() => onAddSaleProduct(item.id)}
         >
           <Ionicons name="add-circle" size={32} color="green" />
         </TouchableOpacity>
@@ -232,6 +293,7 @@ export default function Sales() {
             textAlignVertical: "center",
             paddingBottom: 15,
           }}
+          keyboardType="numeric"
         >
           {item.quantity}
         </TextInput>
@@ -239,56 +301,71 @@ export default function Sales() {
           style={{
             margin: 5,
           }}
-          onPress={() =>
-            onAddProduct({
-              id: item.id,
-              name: item.name,
-              price: item.price,
-              barcode: item.barcode,
-              quantity: 1,
-            })
-          }
+          onPress={() => onRemoveSaleProduct(item.id)}
         >
           <Ionicons name="remove-circle" size={32} color="red" />
         </TouchableOpacity>
       </View>
     );
 
-    const onChangeBuscar = (text) => {
-      if (text === "") {
-        setFilteredProducts([]);
-      } else {
-        setFilteredProducts(
-          products.filter(
-            (product) =>
-              product.name.toLowerCase().includes(text.toLowerCase()) ||
-              product.barcode.toLowerCase().includes(text.toLowerCase())
-          )
-        );
-      }
-    };
     return (
       <View
         style={{
           paddingTop: 0,
           paddingHorizontal: 10,
           width: "100%",
-          maxHeight: 250,
+          maxHeight: "90%",
+          height: "90%",
         }}
       >
         <Text
           style={{
-            fontSize: 18,
+            fontSize: 25,
             fontWeight: "bold",
             textAlign: "center",
+            paddingTop: 10,
           }}
         >
-          Productos Seleccionados
+          Productos Agregados Vendidos:
+          {productsForSales.reduce(
+            (total, product) => total + product.quantity,
+            0
+          )}
         </Text>
         <FlatList
           data={productsForSales}
           renderItem={items}
           keyExtractor={(item) => item.id}
+        />
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: "bold",
+            textAlign: "center",
+            paddingTop: 10,
+          }}
+        >
+          Monto Total: $
+          {productsForSales
+            .reduce(
+              (total, product) => total + product.price * product.quantity,
+              0
+            )
+            .toFixed(2)}
+        </Text>
+        <Button
+          title="Cobrar"
+          onPress={() => {
+            console.log("Cobrar");
+          }}
+          iconName="save"
+        />
+        <Button
+          title="Cancelar"
+          onPress={() => {
+            console.log("Cancelar");
+          }}
+          iconName="save"
         />
       </View>
     );
